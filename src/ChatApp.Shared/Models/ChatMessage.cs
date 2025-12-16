dedf03efd5dab77f235cc37bd2e4660029c513a4
ZodiacMLG <zodiacmlg@bk.ru>
2025-12-16 13:00:42 +0500

    Update files

--- src/ChatApp.Shared/Models/ChatMessage.cs
+++ src/ChatApp.Shared/Models/ChatMessage.cs
@@ -2,6 +2,9 @@
 {
     public class ChatMessage
     {
-
+        public string UserName { get; set; }
+        public string Text { get; set; }
+        public DateTime SentAt { get; set; }
+        public string Room {  get; set; }
     }
 }
