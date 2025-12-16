aa629fb824be0625632b1a878a3fb82aee3c6f3a
ZodiacMLG <zodiacmlg@bk.ru>
2025-12-16 12:42:51 +0500

    Refactoring

---
+++ src/ChatApp.Server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ src/ChatApp.Shared/Models/ChatMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatApp.Shared.Models
+{
+    public class ChatMessage
+    {
+
+    }
+}
